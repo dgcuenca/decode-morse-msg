7c768580bb32d41676205a6843abc749f15bcec4
Javivy <84439052+Javivy@users.noreply.github.com>
2023-03-01 17:21:47 -0400

    create decode_word and decode_phrase functions

--- morseCharacter.rb
+++ morseCharacter.rb
@@ -35,4 +35,27 @@ def decode_char ( morseChar)
   letter
 end
 
-puts decode_char('-..-')
+def decode_word(word)
+  decoded_word = ''
+  new_word = word.split(' ')
+
+  new_word.each do |char|
+    decoded_word += decode_char(char)
+  end
+  decoded_word
+end
+
+def decode_phrase(phrase)
+  decoded_phrase = ''
+  words = phrase.split('   ')
+
+  words.each do |word|
+    decoded_phrase += decode_word(word) + " "
+  end
+  decoded_phrase
+end
+
+puts decode_char('-..-') # Should return X
+puts decode_word("-- -.--") # Should return MY
+puts decode_phrase("-- -.--   -. .- -- .") # Should return MY NAME
+puts decode_phrase(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")

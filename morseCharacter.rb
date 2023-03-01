bc50c6da3a1454e8117080c2262f76c79ca7f048
diego <dgcuenca@gmail.com>
2023-03-01 16:03:58 -0500

    Decode one character

---
+++ morseCharacter.rb
@@ -0,0 +1,38 @@
+LETTERS = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..'
+}.freeze
+
+def decode_char ( morseChar) 
+  letter = ""
+  LETTERS.each do |key,value|
+    letter+=key.to_s if morseChar == value
+  end
+  letter
+end
+
+puts decode_char('-..-')
